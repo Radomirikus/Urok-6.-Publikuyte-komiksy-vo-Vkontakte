4df959cad6d2f0f0c0989d248377e68a27960517
Radomirikus <r2a3d0o3m1i2r@gmail.com>
2024-09-26 18:22:51 +0300

    Добавил def main

--- publish_comics.py
+++ publish_comics.py
@@ -5,18 +5,21 @@ import time
 import argparse
 from dotenv import load_dotenv
 
+def main():
+    load_dotenv()
+    telegram_token = os.environ['TELEGRAM_TOKEN']
 
-load_dotenv()
-telegram_token = os.environ['TELEGRAM_TOKEN']
+    parser = argparse.ArgumentParser(description='Скачивает и публикует комиксы Xkcd про Python')
+    parser.add_argument('--delay_time', type=int, default=216000, help='Задержка между публикованием комиксов, По умолчанию - 1 час')
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description='Скачивает и публикует комиксы Xkcd про Python')
-parser.add_argument('--delay_time', type=int, default=216000, help='Задержка между публикованием комиксов, По умолчанию - 1 час')
-args = parser.parse_args()
+    while True:
+        download_xkcd_comics()
+        bot = telegram.Bot(token=telegram_token)
+        bot.send_document(chat_id='@xkcd_comicss', document=open('comics.png', 'rb'))
+        os.remove("comics.png")
+        time.sleep(args.delay_time)
 
 
-while True:
-    download_xkcd_comics()
-    bot = telegram.Bot(token=telegram_token)
-    bot.send_document(chat_id='@xkcd_comicss', document=open('comics.png', 'rb'))
-    os.remove("comics.png")
-    time.sleep(args.delay_time)
+if __name__ == "__main__":
+    main()
